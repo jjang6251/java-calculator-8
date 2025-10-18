9040eb0576d8113de5f25a0f6733a84eb81b131a
장성원 <jjang6251@gmail.com>
2025-10-18 20:41:19 +0900

    feat(calculator): 기본 구분자(, :) 합산 기능

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,7 +8,13 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
         StringCalculator c = new StringCalculator();
-        int result = c.add(input);
-        System.out.println("결과 : " + result);
+
+        try {
+            int result = c.add(input);
+            System.out.println("결과 : " + result);
+        } catch (IllegalArgumentException e) {
+            System.out.println("[Error] " + e.getMessage());
+        }
+
     }
 }
--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -5,8 +5,19 @@ public class StringCalculator {
         if(input == null || input.isEmpty()) {
             return 0;
         }
-        else {
-            return 1;
+        String[] tokens = input.split("[,:]");
+        int result = 0;
+        for(String x : tokens) {
+            result += toInt(x);
+        }
+        return result;
+    }
+
+    private int toInt(String token) {
+        try {
+            return Integer.parseInt(token);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("기본 구분자 이외의 구분자가 있습니다");
         }
     }
 }
