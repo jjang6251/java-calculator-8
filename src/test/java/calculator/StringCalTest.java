0372a63f20a5529b639cefce37f32bc12dcb39ea
장성원 <jjang6251@gmail.com>
2025-10-18 20:13:17 +0900

    test(calculator): 기본 구분자(, :) 케이스 추가

--- src/test/java/calculator/StringCalTest.java
+++ src/test/java/calculator/StringCalTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class StringCalTest {
@@ -16,5 +17,28 @@ public class StringCalTest {
         assertThat(c.add("")).isEqualTo(0);
         assertThat(c.add(null)).isEqualTo(0);
         assertThat(c.add("1:2:3")).isNotEqualTo(0);
+    }
+
+    @DisplayName("기본 구분자(, :) 합산 성공")
+    @Test
+    void 기본_구분자_합산_성공() {
+        StringCalculator c = new StringCalculator();
+
+        //성공 케이스
+        assertThat(c.add("1,2,3")).isEqualTo(6);
+        assertThat(c.add("1:2:3")).isEqualTo(6);
+        assertThat(c.add("1,2:3")).isEqualTo(6);
+    }
+
+    @DisplayName("기본 구분자(, :) 합산 실패")
+    @Test
+    void 기본_구분자_합산_실패() {
+        StringCalculator c = new StringCalculator();
+
+        //실패 케이스
+        assertThatThrownBy(() -> c.add("1/2,3"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> c.add("1:2/3"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
