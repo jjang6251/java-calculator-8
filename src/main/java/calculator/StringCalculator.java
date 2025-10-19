89bf5d323acc6bff27754ed6daba11466c90d541
장성원 <jjang6251@gmail.com>
2025-10-19 13:07:34 +0900

    feat(calculator): 커스텀 구분자 기능 구현

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,10 +1,28 @@
 package calculator;
 
+import java.util.regex.Pattern;
+
 public class StringCalculator {
     public int add(String input) {
         if(input == null || input.isEmpty()) {
             return 0;
         }
+        if(input.startsWith("//")) {
+            //커스텀 구분자
+            int lineIndex = input.indexOf('\n');
+            if(lineIndex == -1) {
+                throw new IllegalArgumentException("잘못된 커스텀 구분자 형식");
+            }
+            String separator = input.substring(2, lineIndex);
+            String numString = input.substring(lineIndex+1);
+            String[] tokens = numString.split(Pattern.quote(separator));
+            int result = 0;
+            for(String x : tokens) {
+                result += toInt(x);
+            }
+            return result;
+        }
+        // 기본 구분자
         String[] tokens = input.split("[,:]");
         int result = 0;
         for(String x : tokens) {
